7846af28c30096b857d4c26e9b19cb89945c2b68
douggomes404 <60206634+douggomes404@users.noreply.github.com>
2021-02-26 17:40:51 -0300

    criando composer buscacep

---
+++ exemplo.php
@@ -0,0 +1,11 @@
+<?php
+  
+require_once "vendor/autoload.php";
+
+use \Doug\DigitalCep\Search;
+
+$busca = new Search;
+
+$resultado = $busca->getAddressFromZipcode('88085201');
+
+print_r($resultado);
